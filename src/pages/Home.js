a65f2eefa4b5d2253c3ab1aebc97661bf718007b
sntwandy <sntwandy@gmail.com>
2020-05-20 19:58:25 -0400

    Finished error404 page and Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 /// CSS
 import './styles/Home.css';
+
+import platziConfLogo from '../images/platziconf-logo.svg';
+import astronauts from '../images/astronauts.svg';
 
 function Home () {
     return(
         <React.Fragment>
             <div className="Home-background">
+                <div className="Home-content">
+                    <div className="Home-content-conf">
+                        <img className="img-fluid" src={platziConfLogo} alt=""/>
+                        <h2>Print your badges</h2>
+                        <p>The easiest way to manage your conference</p>
+                        <Link className="btn btn-primary" to="/Badges">Start now</Link>
+                    </div>
+                    <div className="Home-astronauts">
+                        <img className="Home-astronauts img-fluid" src={astronauts} alt=""/>
+                    </div>
+                </div>
             </div>
         </React.Fragment>
     );
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+// CSS
+import './styles/Notfound.css'
+
 function NotFound () {
-    return <h1>404: not found</h1>;
+    return(
+        <React.Fragment>
+            <div className="Notfound-container .d-sm-block">
+                <div className="Notfound-title">
+                    <h1>404: not found</h1>
+                </div>
+                <div className="Notfound-image">
+                    <img className="img-fluid" src="https://static.platzi.com/static/images/error/img404.png" alt="Not found logo" />
+                </div>
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default NotFound;
