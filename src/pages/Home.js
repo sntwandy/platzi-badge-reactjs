168397d62c31f2fa382268a5e6b93527d81bcdee
sntwandy <sntwandy@gmail.com>
2020-05-21 09:51:46 -0400

    fixing the Home responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,14 +11,14 @@ function Home () {
     return(
         <React.Fragment>
             <div className="Home-background">
-                <div className="Home-content">
-                    <div className="Home-content-conf">
+                <div className="row Home-content">
+                    <div className="col-sm-8 col-md-5 Home-content-conf">
                         <img className="img-fluid" src={platziConfLogo} alt=""/>
                         <h2>Print your badges</h2>
                         <p>The easiest way to manage your conference</p>
                         <Link className="btn btn-primary" to="/Badges">Start now</Link>
                     </div>
-                    <div className="Home-astronauts">
+                    <div className="col-sm-8 col-md-5 Home-astronauts">
                         <img className="Home-astronauts img-fluid" src={astronauts} alt=""/>
                     </div>
                 </div>
